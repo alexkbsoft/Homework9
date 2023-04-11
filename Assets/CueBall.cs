129f85981a9e4483ff41736170d9954b8e3e643e
alexkbsoft <san.ok@bk.ru>
2023-04-11 10:16:46 +0700

    Billiard ready

---
+++ Assets/CueBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CueBall : MonoBehaviour
+{
+    
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * 15.0f, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
